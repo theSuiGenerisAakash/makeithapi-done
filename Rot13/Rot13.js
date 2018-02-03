17316d54647dd8f41dfd81118a1f2c1a8a84caad
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-03 21:03:22 +0530

    Rot13 - Modded Rot13.js for makemehapi verify

--- Rot13/Rot13.js
+++ Rot13/Rot13.js
@@ -7,16 +7,29 @@ const server = new Hapi.Server();
 
 server.connection({
   host: 'localhost',
-  port: 8080,
+  port: Number(process.argv[2] || 8080),
 });
+
+// const handler = (req, res) => res(fs.createReadStream(path.join(__dirname, 'main.txt'))
+//   .pipe(Rot13()));
 
 const handler = (req, res) => res(fs.createReadStream(path.join(__dirname, req.query.name))
   .pipe(Rot13()));
 
 server.route({
   path: '/file',
+  // path: '/',
   method: 'GET',
   handler,
 });
+
+if (!module.parent) {
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+}
 
 module.exports = server;
